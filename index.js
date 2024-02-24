b0b57dabe7fb7735230c7ef53d1b92148c1a6759
Raul Soneira <raul.soneira@cirvianum.cat>
2024-02-24 12:14:47 +0100

    Configura el formato de fin de línea

---
+++ index.js
@@ -0,0 +1,41 @@
+import swapi from "./swapi.js";
+
+async function principal() {
+  try {
+    // Obté i mostra la informació meteorològica per defecte
+    const dadesMeteorologiquesPerDefecte = await swapi.obtenirMeteorologia("Barcelona");
+    swapi.mostrarInformacioMeteorologica(dadesMeteorologiquesPerDefecte);
+
+    // Filtra la informació meteorològica segons la temperatura mínima
+    if (swapi.filtrarDadesMeteorologiques(dadesMeteorologiquesPerDefecte, 13)) {
+      console.log("Complaix amb el criteri de temperatura mínima.");
+    } else {
+      console.log("No complaix amb el criteri de temperatura mínima.");
+    }
+
+    // Canvia la ciutat i obté nova informació meteorològica
+    const novaCiutat = "Granada";
+    await swapi.canviarCiutatIObtenirMeteorologia(novaCiutat);
+
+    // CRUD
+    // Obtenir i mostrar dades meteorològiques existents
+    const totesLesDadesMeteorologiques = await swapi.obtenirTotesLesDadesMeteorologiques();
+
+    // Crear un nou conjunt de dades meteorològiques
+    // const novesDadesMeteorologiques = await swapi.crearDadesMeteorologiques('Paris', 18, 'Plujós');
+
+    // Actualitzar el nou conjunt de dades meteorològiques
+    // const dadesMeteorologiquesActualitzades = await swapi.modificarDadesMeteorologiques(2, {
+    //   temperatura: 20,
+    //   descripcio: 'Assolellat',
+    // });
+
+    // Eliminar el conjunt de dades meteorològiques actualitzat
+    // await swapi.eliminarDadesMeteorologiques("3");
+
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+principal();
---
+++ swapi.js
@@ -0,0 +1,159 @@
+// Clau de l'API d'OpenWeatherMap
+const OPENWEATHER_API_KEY = "7b3284127183ee66da9db0128c1089d0";
+
+// Funció per obtenir la informació meteorològica d'una ciutat
+async function obtenirMeteorologia(ciutat) {
+  try {
+    const res = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${ciutat}&appid=${OPENWEATHER_API_KEY}`
+    );
+    const dades = await res.json();
+
+    // Crida a la funció per mostrar la informació a l'HTML
+    mostrarInformacioMeteorologica(dades);
+
+    return dades;
+  } catch (error) {
+    throw new Error("Error en obtenir la informació meteorològica.");
+  }
+}
+
+// Funció per canviar la ciutat i obtenir nova informació meteorològica
+async function canviarCiutatIObtenirMeteorologia(novaCiutat) {
+  try {
+    const novesDadesMeteorologiques = await obtenirMeteorologia(novaCiutat);
+    console.log(novesDadesMeteorologiques);
+
+    // Mostra la nova informació a la interfície d'usuari
+    mostrarInformacioMeteorologica(novesDadesMeteorologiques);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Funció per mostrar la informació meteorològica a l'HTML
+function mostrarInformacioMeteorologica(dadesMeteorologiques) {
+  const divInformacioMeteorologica = document.getElementById("weather-info");
+
+  const temperaturaCelsius = dadesMeteorologiques.main.temp - 273.15;
+
+  divInformacioMeteorologica.innerHTML = `
+    <p>Nom de la Ciutat: ${dadesMeteorologiques.name}</p>
+    <p>Temperatura: ${temperaturaCelsius.toFixed(2)} °C</p>
+    <p>Descripció: ${dadesMeteorologiques.weather[0].description}</p>
+  `;
+}
+
+// Funció per filtrar la informació meteorològica segons la temperatura mínima
+function filtrarDadesMeteorologiques(dadesMeteorologiques, temperaturaMinima) {
+  // Obté la temperatura en graus Celsius a partir de les dades meteorològiques
+  const temperaturaCelsius = dadesMeteorologiques.main.temp - 273.15;
+
+  // Comprova si la temperatura és igual o superior a la temperatura mínima especificada
+  return temperaturaCelsius >= temperaturaMinima;
+}
+
+// CRUD
+
+// CREAR
+async function crearDadesMeteorologiques(ciutat, temperatura, descripcio) {
+  try {
+    // Envia una sol·licitud POST per crear noves dades meteorològiques
+    const res = await fetch("http://localhost:3000/weatherData", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        ciutat,
+        temperatura,
+        descripcio,
+      }),
+    });
+
+    // Obté i imprimeix la resposta del servidor
+    const novesDadesMeteorologiques = await res.json();
+    console.log("Nova dada meteorològica creada:", novesDadesMeteorologiques);
+
+    // Retorna les noves dades meteorològiques creades
+    return novesDadesMeteorologiques;
+  } catch (error) {
+    // Captura i gestiona qualsevol error que pugui ocórrer durant la creació de dades meteorològiques
+    console.error("Error en crear la dada meteorològica:", error);
+    throw new Error("Error en crear la dada meteorològica.");
+  }
+}
+
+// LLEGIR
+async function obtenirTotesLesDadesMeteorologiques() {
+  try {
+    // Realitza una sol·licitud GET per obtenir totes les dades meteorològiques
+    const res = await fetch("http://localhost:3000/weatherData");
+    // Obté i imprimeix les dades meteorològiques actuals
+    const dadesMeteorologiques = await res.json();
+    console.log("Dades meteorològiques actuals:", dadesMeteorologiques);
+
+    // Retorna les dades meteorològiques obtingudes
+    return dadesMeteorologiques;
+  } catch (error) {
+    // Captura i gestiona qualsevol error que pugui ocórrer durant la lectura de dades meteorològiques
+    console.error("Error en obtenir les dades meteorològiques:", error);
+    throw new Error("Error en obtenir les dades meteorològiques.");
+  }
+}
+
+// MODIFICAR
+async function modificarDadesMeteorologiques(id, novesDades) {
+  try {
+    // Realitza una sol·licitud PUT per actualitzar les dades meteorològiques amb l'ID especificat
+    const res = await fetch(`http://localhost:3000/weatherData/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(novesDades),
+    });
+
+    // Obté i imprimeix les dades meteorològiques actualitzades
+    const dadesMeteorologiquesActualitzades = await res.json();
+    console.log(
+      "Dada meteorològica actualitzada:",
+      dadesMeteorologiquesActualitzades
+    );
+
+    // Retorna les dades meteorològiques actualitzades
+    return dadesMeteorologiquesActualitzades;
+  } catch (error) {
+    // Captura i gestiona qualsevol error que pugui ocórrer durant la modificació de dades meteorològiques
+    console.error("Error en actualitzar la dada meteorològica:", error);
+    throw new Error("Error en actualitzar la dada meteorològica.");
+  }
+}
+
+// ELIMINAR
+async function eliminarDadesMeteorologiques(id) {
+  try {
+    // Realitza una sol·licitud DELETE per eliminar les dades meteorològiques amb l'ID especificat
+    const res = await fetch(`http://localhost:3000/weatherData/${id}`, {
+      method: "DELETE",
+    });
+
+    // Imprimeix un missatge indicant que les dades meteorològiques han estat eliminades amb èxit
+    console.log("Dada meteorològica eliminada amb èxit.");
+  } catch (error) {
+    // Captura i gestiona qualsevol error que pugui ocórrer durant l'eliminació de dades meteorològiques
+    console.error("Error en eliminar la dada meteorològica:", error);
+    throw new Error("Error en eliminar la dada meteorològica.");
+  }
+}
+
+export default {
+  obtenirMeteorologia,
+  mostrarInformacioMeteorologica,
+  canviarCiutatIObtenirMeteorologia,
+  filtrarDadesMeteorologiques,
+  crearDadesMeteorologiques,
+  obtenirTotesLesDadesMeteorologiques,
+  modificarDadesMeteorologiques,
+  eliminarDadesMeteorologiques,
+};
